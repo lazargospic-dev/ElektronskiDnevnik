c64ca5a8500b1e76c46072b5d039d0fa8526b330
lazargospic-dev <lazar.gospic@prvabeogim.edu.rs>
2026-04-08 15:52:20 +0200

    Add project files.

---
+++ sifarnik.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace dnevnik410a
+{
+    public partial class sifarnik : Form
+    {
+        SqlDataAdapter Adapter;
+        DataTable podaci;
+        string ime_tabele;
+        public sifarnik(string tabela)
+        {
+            ime_tabele = tabela;
+            InitializeComponent();
+        }
+
+        private void sifarnik_Load(object sender, EventArgs e)
+        {
+            Adapter = new SqlDataAdapter("SELECT * FROM "+ime_tabele, konekcija.povezi());
+            podaci = new DataTable();
+            Adapter.Fill(podaci);
+            dataGridView1.DataSource = podaci;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataTable menjano = podaci.GetChanges();
+            // DataRowState.Modified = promenjeni
+            // DataRowState.Added    = dodati
+            // DataRowState.Deleted  = obrisani
+            // DataRowState.Unchanged= nepromenjeni
+            Adapter.UpdateCommand = new SqlCommandBuilder(Adapter).GetUpdateCommand();
+            if (menjano != null)
+            {
+                Adapter.Update(menjano);
+                this.Close();
+            }
+            else
+                this.Close();
+        }
+    }
+}
